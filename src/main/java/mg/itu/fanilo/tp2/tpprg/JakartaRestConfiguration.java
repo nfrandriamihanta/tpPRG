f799acedbf6d335b3d41658aa1b528a30c82e288
Ny Aina Fanilo <nfrandriamihanta@gmail.com>
2023-04-01 22:13:02 +0300

    Exercice 1 : analyse d'un post tout simple

---
+++ src/main/java/mg/itu/fanilo/tp2/tpprg/JakartaRestConfiguration.java
@@ -0,0 +1,13 @@
+package mg.itu.fanilo.tp2.tpprg;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+/**
+ * Configures Jakarta RESTful Web Services for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JakartaRestConfiguration extends Application {
+    
+}
